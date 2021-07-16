222e8b0eaa2b4fc8f8c88e4649c4c2d5a88d8592
devduhariya <64675676+devduhariya@users.noreply.github.com>
2021-07-16 11:42:03 +0530

    intial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+// import mongoose from 'mongoose';
+// import path from 'path';
+// import cors from 'cors';
+// import bodyParser from 'body-parser';
+// import morgan from 'morgan';
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("app is running well")
+})
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 8900
+
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
