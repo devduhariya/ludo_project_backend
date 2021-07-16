7a6f836c3159b5844173725b8da27729b5409ffe
AarushiGaur <aarushigaur91@gmail.com>
2021-07-16 15:02:06 +0530

    routes

---
+++ routes/api/auth.js
@@ -0,0 +1,114 @@
+import { Router } from 'express';
+import bcrypt from 'bcryptjs';
+//import config from '../../config';
+import jwt from 'jsonwebtoken';
+//import auth from '../../middleware/auth';
+// User Model
+require ('dotenv').config();
+import User from '../../models/User'
+
+const JWT_SECRET   = process.env.JWT_SECRET;
+const router = Router();
+
+/**
+ * @route   POST api/auth/login
+ * @desc    Login user
+ */
+
+router.post('/login', async (req, res) => {
+  const { ph, password } = req.body;
+
+  try {
+    // Check for existing user
+    const user = await User.findOne({ ph });
+    if (!user) throw Error('User does not exist');
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) throw Error('Invalid credentials');
+
+    const token = jwt.sign({ user }, JWT_SECRET);
+    if (!token) throw Error('Couldnt sign the token');
+
+
+    res.status(200).json({
+      token: token,
+      user: {
+        id: user._id,
+        name: user.name,
+        ph: user.ph,
+        role : user.role
+      }
+    });
+    console.log(token);
+  } catch (e) {
+    res.status(400).json({ msg: e.message });
+  }
+});
+
+/**
+ * @route   POST api/users
+ * @desc    Register new user
+ */
+
+router.post('/register', async (req, res) => {
+  const { name, ph, email, password } = req.body;
+  try {
+    const user = await User.findOne({ ph });
+    if (user) throw Error('User already exists');
+
+    const salt = await bcrypt.genSalt(10);
+    if (!salt) throw Error('Something went wrong with bcrypt');
+
+    const hash = await bcrypt.hash(password, salt);
+    if (!hash) throw Error('Something went wrong hashing the password');
+
+    const newUser = new User({
+      name,
+      ph,
+      email,
+      password: hash
+    });
+    newUser.role = "user"
+    const savedUser = await newUser.save();
+    // console.log("user",savedUser);
+    if (!savedUser) throw Error('Something went wrong saving the user');
+
+    const token = jwt.sign({id:savedUser.id}, JWT_SECRET, {
+      expiresIn: "5m"
+    });
+    const role = savedUser.role;
+    res.status(200).json({
+      token,
+      role,
+      user: {
+        id: savedUser.id,
+        name: savedUser.name,
+        ph: savedUser.ph,
+        email: savedUser.email,
+        role: savedUser.role
+      }
+    });
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
+});
+
+/**
+ * @route   GET api/auth/user
+ * @desc    Get user data
+ */
+
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find();
+    if (!users) throw Error('No queries');
+
+    res.status(200).json(users);
+  } catch (e) {
+    res.status(400).json({ msg: e.message });
+  }
+});
+
+
+
+export default router;
---
+++ routes/api/query.js
@@ -0,0 +1,53 @@
+import { Router } from 'express';
+//import config from '../../config';
+import jwt from 'jsonwebtoken';
+import Query from '../../models/Query';
+import auth from '../../middleware/auth';
+
+const router = Router();
+const  JWT_SECRET  = process.env.JWT_SECRET;
+
+/**
+ * @route   GET api/query
+ * @desc    Get All queries
+ * @access  Public
+ */
+
+router.get('/', async (req, res) => {
+  try {
+    const query = await Query.find();
+    if (!query) throw Error('No queries');
+
+    res.status(200).json(query);
+  } catch (e) {
+    res.status(400).json({ msg: e.message });
+  }
+});
+
+router.post('/', auth, async (req, res) => {
+  const { whatsapp, paytm, txn_Id, reciver_Paytm, amount, message, screenshots } = req.body;
+  jwt.verify(req.token, JWT_SECRET, async (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      try {
+        const newQuery = new Query({
+          whatsapp,
+          paytm,
+          reciver_Paytm,
+          txn_Id,
+          amount,
+          message,
+          screenshots
+        });
+        const query = await newQuery.save();
+        if (!query) throw Error('Something went wrong saving the query');
+        res.status(200).json({ authData, query });
+      } catch (e) {
+        return res.status(400).json({ msg: e.message });
+      }
+
+    }
+  });
+});
+export default router;
