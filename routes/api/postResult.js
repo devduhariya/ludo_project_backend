9f93aceee5cfffbc59ab84d5c17fcd2adce94737
devduhariya <64675676+devduhariya@users.noreply.github.com>
2021-08-07 12:19:32 +0530

    post change

--- routes/api/postResult.js
+++ routes/api/postResult.js
@@ -98,7 +98,7 @@ module.exports = (app) => {
             const userId = ans._id;
             let user2 = ans.paytm_no
             const currentUserAmount = ans.amount
-            const { won,lost} = req.body
+            const { won} = req.body
 
             if (err) {
                 res.sendStatus(403);
@@ -118,7 +118,6 @@ module.exports = (app) => {
                         user2: {
                             user2,
                             won,
-                            lost,
                             screenshots: url + '/public/' + req.file.filename
                         },
 
@@ -140,7 +139,7 @@ module.exports = (app) => {
 
         const id = req.params.id;
         const url = req.protocol + '://' + req.get('host');
-        const { won,lost} = req.body
+        const { won} = req.body
 
 
         jwt.verify(req.token, JWT_SECRET, async (err, authData) => {
@@ -152,7 +151,6 @@ module.exports = (app) => {
                     user1: {
                         user1,
                         won,
-                        lost,
                         screenshots: url + '/public/' + req.file.filename
                     },
                 },
@@ -180,6 +178,105 @@ module.exports = (app) => {
         });
     });
 
+
+
+
+
+
+
+
+
+
+
+    // const subtractChips = function (a, b) {
+    //     return a - b;
+    // }
+    // const TotalAmount = (a) => a + a
+
+    // app.post('/api/result/:id', auth, async (req, res) => {
+    //     const id = req.params.id;
+    //     // const url = req.protocol + '://' + req.get('host')
+    //     jwt.verify(req.token, JWT_SECRET, async (err, authData) => {
+    //         const product = await Challenge.findById({ _id: id })
+
+    //         console.log("user1", product)
+    //         const challengeAmount = product.amount
+
+    //         const ans = await Payment.findOne({ paytm_no: authData.user.ph })
+    //         const userId = ans._id;
+    //         let user2 = ans.paytm_no
+    //         const currentUserAmount = ans.amount
+    //         const { won,lost} = req.body
+
+    //         if (err) {
+    //             res.sendStatus(403);
+    //         }
+    //         else {
+    //             if (challengeAmount > currentUserAmount) {
+    //                 res.status(400).json({ message: 'insufficient chips' });
+    //             } else {
+    //                 const ans = await Payment.findByIdAndUpdate(userId,
+    //                     {
+    //                         amount: subtractChips(currentUserAmount, challengeAmount)
+
+    //                     },
+    //                     { new: true }
+    //                 );
+    //                 const newResult = new Result({
+    //                     user2: {
+    //                         user2,
+    //                         won,
+    //                         lost,
+    //                         // screenshots: url + '/public/' + req.file.filename
+    //                     },
+
+    //                     challengeAmount: TotalAmount(challengeAmount)
+
+    //                 });
+    //                 console.log(newResult);
+    //                 const GameResult = await newResult.save();
+    //                 if (!GameResult) throw Error('Something went wrong saving the challenge');
+    //                 res.status(200).json({ GameResult });
+
+    //             }
+    //         }
+    //     });
+
+    // });
+
+    // app.put('/api/result/:id', auth, async (req, res) => {
+
+    //     const id = req.params.id;
+    //     // const url = req.protocol + '://' + req.get('host');
+    //     const { won,lost} = req.body
+
+
+    //     jwt.verify(req.token, JWT_SECRET, async (err, authData) => {
+    //         const product = await Result.findById({ _id: id })
+    //         const user1 = authData.user.ph
+    //         console.log("product", product.user1)
+    //         const result1 = await Result.findByIdAndUpdate(id,
+    //             {
+    //                 user1: {
+    //                     user1,
+    //                     won,
+    //                     lost,
+    //                     // screenshots: url + '/public/' + req.file.filename
+    //                 },
+    //             },
+    //             { new: true }
+    //         );
+    //         res.send(result1);
+    //         // }
+    //     });
+
+    // });
+
+
+
+
+
+
     const addWiningAmount = (a, b) => a + b
 
     app.put('/api/result/winner/:id', auth, async (req, res) => {
@@ -194,9 +291,11 @@ module.exports = (app) => {
                     const user1Number = chips.user1[0].user1
                     const user2Number = chips.user2[0].user2
                     const winingAmount = chips.challengeAmount;
-                    const user1Status = chips.user1[0].result
-                    const user2Status = chips.user2[0].result
-                    if (user1Status === "won" && user2Status === "lost") {
+                    const user1Status = chips.user1[0].won
+                    // const user1Status = chips.user1[0].lost
+                    const user2Status = chips.user2[0].won
+                    // const user2Status = chips.user2[0].lost
+                    if (user1Status ==true && user2Status ==false) {
                         const chipsUserHave = await Payment.findOne({ paytm_no: user1Number })
                         console.log("chipsUserHave", chipsUserHave)
                         const existAmount = chipsUserHave.amount
@@ -208,7 +307,7 @@ module.exports = (app) => {
                         );
                         res.json(UserOneTotalAmount)
                     }
-                    else if (user2Status === "won" && user1Status === "lost") {
+                    else if (user2Status == true && user1Status ==false) {
                         const chipsUserHave = await Payment.findOne({ paytm_no: user2Number })
                         console.log("chipsUserHave", chipsUserHave)
                         // const user2Id = chipsUserHave_id
@@ -222,7 +321,7 @@ module.exports = (app) => {
                         // console.log("UserTwoTotalAmount", UserTwoTotalAmount)
                         res.json(UserTwoTotalAmount)
                     }
-                    else if(user2Status === "won" && user1Status === "won" || user2Status === "lost" && user1Status === "lost"){
+                    else if(user2Status ==true && user1Status ==true || user2Status == false && user1Status == false){
                         const Admin = 7357525272
                         let chips = await Payment.findOne({ paytm_no: Admin });
                         let AdminId = chips._id
